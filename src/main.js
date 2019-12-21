df19b6fab84b04d7cfc1b38c7fe39547729092c3
xuzhongkang <2508003507@qq.com>
2019-12-21 17:23:58 +0800

    refactor:增加注释

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import 'iview/dist/styles/iview.css'
 import iView from 'iview';
 // 引用axios，并设置基础URL为后端服务api地址
 import axios from "axios";
-axios.defaults.baseURL = 'https://localhost:8888/api'
+// axios.defaults.baseURL = 'https://localhost:8888/api'
 
-// 将API方法绑定到全局
-Vue.prototype.$axios = axios
+// 将API方法绑定到全局,把axios挂载到vue构造函数的prototype属性上，组件中通过this.axios来拿到axios
+Vue.prototype.axios = axios
+//消息提示的环境配置
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
